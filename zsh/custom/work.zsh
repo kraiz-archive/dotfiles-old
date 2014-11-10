cc50908d21ffe3d199e558b4eb1c134e91e78500
kraiz <lars.kreisz@gmail.com>
2014-11-10 21:28:22 +0100

    removed empty line

--- zsh/custom/work.zsh
+++ zsh/custom/work.zsh
@@ -2,6 +2,6 @@ if [[ `hostname` == W4DEUMSY9002036 ]]; then
     export http_proxy="http://proxy.mms-dresden.de:8080"
     export https_proxy=$http_proxy
     export ftp_proxy=$http_proxy
-    
+
     alias poedit="/cygdrive/c/Program\ Files\ \(x86\)/Poedit/Poedit.exe"
 fi
