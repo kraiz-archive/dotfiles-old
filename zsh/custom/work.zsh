6c064a3cd01880cf214cd9207749ba7b78c770ac
Lars Kreisz <kraiz@users.noreply.github.com>
2014-11-11 08:08:39 +0100

    added project starting tmux scripts

--- zsh/custom/work.zsh
+++ zsh/custom/work.zsh
@@ -4,4 +4,15 @@ if [[ `hostname` == W4DEUMSY9002036 ]]; then
     export ftp_proxy=$http_proxy
 
     alias poedit="/cygdrive/c/Program\ Files\ \(x86\)/Poedit/Poedit.exe"
+
+    vms() {
+        tmux send-keys 'vagrant ssh app -- -t "cd /usr/local/*suite*; webcastsuite/bin/manage runserver 8100" ";" exec /bin/bash' C-m
+        tmux split-window -c "$PWD"
+        tmux send-keys 'vagrant ssh app -- -t "cd /usr/local/*suite*; webcastsuite/bin/manage runcelery -Bv2 worker" ";" exec /bin/bash' C-m
+    }
+    vws() {
+        tmux send-keys 'vagrant ssh app -- -t "cd /usr/local/*suite*; webcastsuite/bin/manage runserver 8100" ";" exec /bin/bash' C-m
+        tmux split-window -c "$PWD"
+        tmux send-keys 'vagrant ssh app -- -t "cd /usr/local/*suite*; webcastsuite/bin/manage runcelery -Bv2 worker" ";" exec /bin/bash' C-m
+    }
 fi
