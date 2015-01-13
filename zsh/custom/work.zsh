efbd522114978cd9dc50d7bed3738147c9bdfab5
Lars Kreisz <kraiz@users.noreply.github.com>
2015-01-13 07:55:43 +0100

    Update work.zsh

--- zsh/custom/work.zsh
+++ zsh/custom/work.zsh
@@ -1,4 +1,8 @@
 if [[ `hostname` == W4DEUMSY9002036 ]]; then
+    export PATH="$HOME/.pyenv/bin:$PATH"
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+
     export http_proxy="http://proxy.mms-dresden.de:8080"
     export https_proxy=$http_proxy
     export ftp_proxy=$http_proxy
