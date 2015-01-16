5ceb45d371f22d90466590a3ef6dc43eb0904f04
Lars Kreisz <kraiz@users.noreply.github.com>
2015-01-16 09:24:08 +0100

    Update vagrant.zsh

--- zsh/custom/vagrant.zsh
+++ zsh/custom/vagrant.zsh
@@ -5,3 +5,9 @@ alias vh='vagrant halt'
 alias vus='vagrant up && vagrant ssh'
 alias vrs='vagrant reload && vagrant ssh'
 alias vst='vagrant status'
+
+function vdi() {
+    vagrant up $1
+    vagrant ssh -c "curl -L https://raw.githubusercontent.com/mmrose/dotfiles/master/install.sh | sh"  $1
+    vagrant ssh -c "sudo chsh -s /bin/zsh vagrant" $1
+}
