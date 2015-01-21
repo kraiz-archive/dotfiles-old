e9caa3d5ef10fb4ea2f8b06d0fdea3176033057b
Lars Kreisz <kraiz@users.noreply.github.com>
2015-01-21 14:05:21 +0100

    Update work.zsh

--- zsh/custom/work.zsh
+++ zsh/custom/work.zsh
@@ -6,6 +6,9 @@ if [[ `hostname` == W4DEUMSY9002036 ]]; then
     export http_proxy="http://proxy.mms-dresden.de:8080"
     export https_proxy=$http_proxy
     export ftp_proxy=$http_proxy
+    
+    export VBOX_USER_HOME='C:/Users/lakr/.VirtualBox'
+    export VAGRANT_HOME='C:/Users/lakr/.vagrant.d'
 
     alias poedit="/cygdrive/c/Program\ Files\ \(x86\)/Poedit/Poedit.exe"
 
