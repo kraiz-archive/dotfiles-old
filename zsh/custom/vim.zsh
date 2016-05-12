ea14851e4462aa510689fb5c2f288844bf563af8
lakr <lars.kreisz@t-systems.com>
2016-05-12 09:41:03 +0200

    pull shallow clone for compiling vim

--- zsh/custom/vim.zsh
+++ zsh/custom/vim.zsh
@@ -1,14 +1,14 @@
 compile-vim() {
     cd /tmp
-    git clone https://github.com/vim/vim.git
+    git clone --depth=1 https://github.com/vim/vim.git
     cd vim
     ./configure --enable-fail-if-missing \
                 --with-features=huge \
                 --enable-multibyte \
                 --enable-perlinterp \
                 --enable-luainterp \
-                --enable-pythoninterp --with-python-config-dir=`echo /usr/lib/python2*/config-*` \
-                --enable-python3interp --with-python3-config-dir=`echo /usr/lib/python3*/config-*` \
+                --enable-pythoninterp=dynamic --with-python-config-dir=`echo /usr/lib/python2*/config-*` \
+                --enable-python3interp=dynamic --with-python3-config-dir=`echo /usr/lib/python3*/config-*` \
                 --prefix=$HOME/.local && \
     make && make install
     cd $HOME
