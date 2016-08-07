a271e8ac36821711b2c1d0b661181d878236db60
kraiz <lars.kreisz@gmail.com>
2016-08-07 17:13:28 +0200

    added requirements to compile my vim

--- zsh/custom/vim.zsh
+++ zsh/custom/vim.zsh
@@ -1,4 +1,5 @@
 compile-vim() {
+    sudo aptitude install python-dev python3-dev libluajit-5.1-dev libperl-dev
     cd /tmp
     git clone --depth=1 https://github.com/vim/vim.git
     cd vim
@@ -7,6 +8,7 @@ compile-vim() {
                 --enable-multibyte \
                 --enable-perlinterp \
                 --enable-luainterp \
+                --with-luajit \
                 --enable-pythoninterp=dynamic --with-python-config-dir=`echo /usr/lib/python2*/config-*` \
                 --enable-python3interp=dynamic --with-python3-config-dir=`echo /usr/lib/python3*/config-*` \
                 --prefix=$HOME/.local && \
